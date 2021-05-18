8993d7a5e4076ea1e7bb4dd32773b9690e814153
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-05-18 15:37:38 +0700

    belajar php composer library

---
+++ src/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar;
+
+class Customer
+{
+    public function __construct(private string $name)
+    {
+    }
+
+    public function sayHello(string $name): string
+    {
+        return "Hello $name, My Name is $this->name";
+    }
+}
